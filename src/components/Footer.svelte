37b69d27a5da0ab8989f9ae3f70e52196b7613f1
jamestinman <james.leeds@gmail.com>
2024-01-29 10:26:44 +0000

    Blog

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,8 +1,12 @@
 <hr />
 <div class="mx-auto justify-center flex flex-row gap-10 p-5 bg-shblue h-full text-sm">
-  <p>Download the code for this blog from GitHub at 
-    <a href="https://github.com/webuildsociety/svelte-headless" target="_new">
-      https://github.com/webuildsociety/svelte-headless
-    </a>
-  </p>
+  <a href="/">
+    Home
+  </a>
+  <a href="/blog">
+    Blog
+  </a>
+  <a href="https://rummij.com/lushjunk">
+    Shop
+  </a>
 </div>
